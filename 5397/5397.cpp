ea4a20e3c1144d32d6325ce530a96e28ca7c14b4
sunsetfactory <yang1194us@gmail.com>
2024-03-18 23:37:04 +0900

    Automated commit at Mon Mar 18 23:37:04 KST 2024

--- 5397/5397.cpp
+++ 5397/5397.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #define cin std::cin
 #define cout std::cout
 #define endl std::endl
 #define string std::string
+#define list std::list
 
 int main() {
     int n;
     cin >> n;
-    int arr[n];
-    int dp[n];
+    string s[n];
+    list<string> l;
+
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << endl;
     }
     return 0;
 }
