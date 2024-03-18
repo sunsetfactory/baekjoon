b3b213d230d7452287530b876103b57a03ea4bdc
sunsetfactory <yang1194us@gmail.com>
2024-03-18 23:32:00 +0900

    Automated commit at Mon Mar 18 23:32:00 KST 2024

---
+++ 5397/5397.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define cin std::cin
+#define cout std::cout
+#define endl std::endl
+#define string std::string
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int dp[n];
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+    }
+    return 0;
+}
