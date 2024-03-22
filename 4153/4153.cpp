b86793b9f71252e96fee5de6417145171db2fdc4
sunsetfactory <yang1194us@gmail.com>
2024-03-22 18:34:21 +0900

    Automated commit at Fri Mar 22 18:34:20 KST 2024

--- 4153/4153.cpp
+++ 4153/4153.cpp
@@ -4,57 +4,22 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    while (n != 0)
+    while (1)
     {
-        int _max = 0;
+        int _max;
         int a, b, c;
         cin >> a >> b >> c;
+        if (a == 0 && b == 0 && c == 0)
+            break;
         _max = max(a, max(b, c));
         if (_max == a && b * b + c * c == a * a)
-        {
-        }
-        else if (_max == b && a * a + c * c == b * b)
-        {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        else if (_max == c && a * a + b * b == c * c)
-        {
-            int temp = a;
-            a = c;
-            c = temp;
-        }
-        else if (a == 0)
-        {
-            a = _max;
-        }
-        else if (b == 0)
-        {
-            b = _max;
-        }
-        else if (c == 0)
-        {
-            c = _max;
-        }
-        {
-            b = 0;
-        }
-        else
-        {
-            c = 0;
-        }
-        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
-        {
             cout << "right" << endl;
-        }
+        else if (_max == b && a * a + c * c == b * b)
+            cout << "right" << endl;
+        else if (_max == c && a * a + b * b == c * c)
+            cout << "right" << endl;
         else
-        {
             cout << "wrong" << endl;
-        }
-        n--;
     }
     return 0;
 }
