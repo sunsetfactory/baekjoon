23c9e086931cf7b7980de7244d7dfec037714ce9
sunsetfactory <yang1194us@gmail.com>
2024-03-22 18:29:19 +0900

    Automated commit at Fri Mar 22 18:29:19 KST 2024

--- 4153/4153.cpp
+++ 4153/4153.cpp
@@ -8,8 +8,44 @@ int main()
     cin >> n;
     while (n != 0)
     {
+        int _max = 0;
         int a, b, c;
         cin >> a >> b >> c;
+        _max = max(a, max(b, c));
+        if (_max == a && b * b + c * c == a * a)
+        {
+        }
+        else if (_max == b && a * a + c * c == b * b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        else if (_max == c && a * a + b * b == c * c)
+        {
+            int temp = a;
+            a = c;
+            c = temp;
+        }
+        else if (a == 0)
+        {
+            a = _max;
+        }
+        else if (b == 0)
+        {
+            b = _max;
+        }
+        else if (c == 0)
+        {
+            c = _max;
+        }
+        {
+            b = 0;
+        }
+        else
+        {
+            c = 0;
+        }
         if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
         {
             cout << "right" << endl;
