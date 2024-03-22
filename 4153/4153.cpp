03304c1bc10ec4addec8fdb8a6f5378a8e0c2b26
sunsetfactory <yang1194us@gmail.com>
2024-03-22 13:58:25 +0900

    Automated commit at Fri Mar 22 13:58:24 KST 2024

---
+++ 4153/4153.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n != 0) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+            cout << "right" << endl;
+        } else {
+            cout << "wrong" << endl;
+        }
+        n--;
+    }
+
+    return 0;
+}
