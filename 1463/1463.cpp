38dd27743133d0d4c2b6219a8637d6a6f15d15fd
sunsetfactory <yang1194us@gmail.com>
2024-03-23 17:17:18 +0900

    Automated commit at Sat Mar 23 17:17:17 KST 2024

--- 1463/1463.cpp
+++ 1463/1463.cpp
@@ -5,6 +5,23 @@ using namespace std;
 int main()
 {
     int n;
+    int cnt = 0;
     cin >> n;
-    while ()
+    while (n != 1)
+    {
+        if (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        else if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n -= 1;
+        }
+        cnt++;
+    }
+    cout << cnt << endl;
 }
