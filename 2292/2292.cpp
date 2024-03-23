31ceaa7544135bf89719b4f8996419cf351e7388
sunsetfactory <yang1194us@gmail.com>
2024-03-23 14:30:04 +0900

    Automated commit at Sat Mar 23 14:30:04 KST 2024

--- 2292/2292.cpp
+++ 2292/2292.cpp
@@ -6,9 +6,22 @@ using namespace std;
 int main()
 {
     int n;
-    int result;
+    int result = 1;
     cin >> n;
-    
+    if (n == 1)
+    {
+        cout << result << endl;
+        return 0;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        result += i * RULE;
+        if (n <= result)
+        {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
     cout << result << endl;
     return 0;
 }
