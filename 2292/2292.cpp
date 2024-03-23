7d3a474f256fed4db40803c3f020509fefeac1d4
sunsetfactory <yang1194us@gmail.com>
2024-03-23 14:18:06 +0900

    Automated commit at Sat Mar 23 14:18:06 KST 2024

---
+++ 2292/2292.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int result;
+    cin >> n;
+    result = 1 + n / 6
+
+                     cout
+             << result << endl;
+    return 0;
+}
