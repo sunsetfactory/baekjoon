ac6df46dacfb3f30b5337b30057da361f84f6cf5
sunsetfactory <yang1194us@gmail.com>
2024-03-23 13:52:59 +0900

    Automated commit at Sat Mar 23 13:52:59 KST 2024

--- 2231/2231.cpp
+++ 2231/2231.cpp
@@ -2,11 +2,25 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    int result = 0;
+    int sum = 0;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        result 
-    }   
+    for (int i = 1; i <= n; i++)
+    {
+        sum = i;
+        while (i)
+        {
+            sum += i % 10;
+            i /= 10;
+            printf("sum: %d\n", sum);
+        }
+        if (sum == n)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
 }
