6adb1c1cfd73c791499d2ae4e275a0c1e2d98ad9
sunsetfactory <125588840+sunsetfactory@users.noreply.github.com>
2024-03-23 13:57:01 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    int temp = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = i;
+        temp = i;
+        while (temp > 0)
+        {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (sum == n)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+    return 0;
+}
