96b423a06062a1019ab3c29eb5c97b2070190b12
sunsetfactory <yang1194us@gmail.com>
2024-03-25 15:50:55 +0900

    Automated commit at Mon Mar 25 15:50:55 KST 2024

--- 1463/1463.cpp
+++ 1463/1463.cpp
@@ -5,10 +5,26 @@ using namespace std;
 
 int dp[1000001];
 
+int solve(int n)
+{
+    if (n == 1)
+        return 0;
+    if (dp[n] > 0)
+        return dp[n];
+
+    dp[n] = solve(n - 1) + 1;
+    if (n % 2 == 0)
+        dp[n] = min(dp[n], solve(n / 2) + 1);
+    if (n % 3 == 0)
+        dp[n] = min(dp[n], solve(n / 3) + 1);
+
+    return dp[n];
+}
+
 int main()
 {
     int n;
     cin >> n;
 
-    solve(n, dp
+    solve(n);
 }
