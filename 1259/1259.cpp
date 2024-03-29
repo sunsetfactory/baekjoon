2485e16398838d75ce12888096ad2ac1efeae467
sunsetfactory <yang1194us@gmail.com>
2024-03-29 17:37:19 +0900

    Automated commit at Fri Mar 29 17:37:19 KST 2024

--- 1259/1259.cpp
+++ 1259/1259.cpp
@@ -8,7 +8,8 @@ int main()
     int first_digit, last_digit;
     int temp;
     int digit;
-    int num = 1;
+    int n_digit = 1;
+    int sign = 0;
 
     while (1)
     {
@@ -22,22 +23,21 @@ int main()
             while (temp > 0)
             {
                 temp /= 10;
-                digit++;
-                cout << "digit: " << digit << endl;
+                n_digit * 10;
             }
+            n_digit * temp;
             first_digit = temp;
             last_digit = n % 10;
-            n = n % num;
+            n = n - n_digit;
             n = n / 10;
-            num = 1;
-
             if (first_digit != last_digit)
             {
                 cout << "no" << endl;
-                break;
+                sign = 1;
+                break ;
             }
         }
-        if (n < 10)
+        if (n < 10 && sign != 1)
         {
             cout << "yes" << endl;
         }
