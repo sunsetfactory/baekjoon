dd228d72f81b64ebaa8190b7b373a89f6194c6f6
sunsetfactory <yang1194us@gmail.com>
2024-04-23 22:03:54 +0900

    Automated commit at Tue Apr 23 22:03:53 KST 2024

--- solved/1259/1259.cpp
+++ solved/1259/1259.cpp
@@ -9,40 +9,37 @@ int main()
     int temp;
     int digit;
     int n_digit;
-    int sign = 0;
+    int sign;
 
-    while (1)
+    while (1) //  loop for input
     {
         sign = 0;
         cin >> n;
         if (n == 0)
             break;
-        while (n >= 10)
+        temp = n;
+        n_digit = 1;
+        while (temp > 9)
         {
-            temp = n;
-            digit = 0;
-            n_digit = 1;
-            while (temp > 9)
-            {
-                temp /= 10;
-                n_digit *= 10;
-            }
-            n_digit *= temp;
-            first_digit = temp;
+            temp /= 10;
+            n_digit *= 10;
+        }
+        while (n_digit > 9)
+        {
+            first_digit = n / n_digit;
             last_digit = n % 10;
-            n = n - n_digit;
-            n = n / 10;
             if (first_digit != last_digit)
             {
                 cout << "no" << endl;
                 sign = 1;
                 break;
             }
+            n = n % n_digit;
+            n = n / 10;
+            n_digit /= 100;
         }
-        if (n < 10 && sign == 0)
-        {
+        if (sign == 0)
             cout << "yes" << endl;
-        }
     }
     return 0;
 }
