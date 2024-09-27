e892afd4f5e8aa5ebc23d960e911f33355f81e1c
seokjyan <125588840+sunsetfactory@users.noreply.github.com>
2024-09-27 16:22:26 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int cnt[4] = {4, 2, 1};
+int level[4] = {210, 220, 230, 240};
+
+int main(void)
+{
+	int N;
+	cin >> N;
+
+	int item = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		// cout << level[i] << " - " << N << " > " << cnt[i] << endl;
+		if ((level[i] - N) > cnt[i])
+		{
+			item = i + 1;
+			break;
+		}
+	}
+	if (item == 0)
+	{
+		cout << "4" << endl;
+	}
+	else
+	{
+		cout << item << endl;
+	}
+}
